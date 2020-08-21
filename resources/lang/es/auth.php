c676cfef2c164e8809595c9f973464c9d0f80b05
juansemarquez <juansemarquez@yahoo.com.ar>
2020-08-21 13:04:47 -0300

    Localización al castellano de Auth

---
+++ resources/lang/es/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Correo electrónico o contraseña incorrectos',
+    'throttle' => 'Intentaste demasiadas veces. Por favor, esperá :seconds segundos.',
+    'Login' => 'Iniciar sesión'
+
+];
